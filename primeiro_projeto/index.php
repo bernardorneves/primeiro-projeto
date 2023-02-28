9578e8882728dbb4d8526f12fa581671cd129bb6
bernardoneves <99209472+bernardorneves@users.noreply.github.com>
2023-02-28 10:19:10 -0300

    Add files via upload

---
+++ primeiro_projeto/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "ola mundo";
+        ?>
+    </h1>
+
+    <h2>1+2=
+        <?php
+            echo(1+2)
+        ?>
+    </h2>
+
+    <h2>5+6=
+        <?php
+            echo(5+6)
+        ?>
+    </h2>
+
+    <h2>9-3=
+        <?php
+            echo(9-3)
+        ?>
+    </h2>
+
+    <h2>7+2=
+        <?php
+            echo(7+2)
+        ?>
+    </h2>
+</body>
+</html>
